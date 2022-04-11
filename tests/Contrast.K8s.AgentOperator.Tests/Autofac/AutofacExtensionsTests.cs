a1f61327728a1044c53ae8646ae888d761c0385c
Mark Lopez <m@silvenga.com>
2022-04-11 11:06:23 -0500

    Added coverage to AutofacExtensions.

--- tests/Contrast.K8s.AgentOperator.Tests/Autofac/AutofacExtensionsTests.cs
+++ tests/Contrast.K8s.AgentOperator.Tests/Autofac/AutofacExtensionsTests.cs
@@ -8,21 +8,47 @@ namespace Contrast.K8s.AgentOperator.Tests.Autofac
     public class AutofacExtensionsTests
     {
         [Fact]
-        public void METHOD_NAME()
+        public void When_contrast_conventions_are_applied_then_Resolve_should_succeed_on_concrete_types()
         {
             var builder = new ContainerBuilder();
 
             // Act
             builder.ApplyContrastConventions(typeof(AutofacExtensionsTests).Assembly);
 
-
             // Assert
             var container = builder.Build();
             container.Resolve<ConcreteClassFixture>().Should().NotBeNull();
         }
 
+        [Fact]
+        public void When_contrast_conventions_are_applied_then_Resolve_should_succeed_on_default_interfaces()
+        {
+            var builder = new ContainerBuilder();
+
+            // Act
+            builder.ApplyContrastConventions(typeof(AutofacExtensionsTests).Assembly);
+
+            // Assert
+            var container = builder.Build();
+            container.Resolve<IDefaultInterfaceFixture>().Should().NotBeNull();
+        }
+
+        // ReSharper disable MemberCanBePrivate.Global
+        // ReSharper disable ClassNeverInstantiated.Global
+        // ReSharper disable UnusedType.Global
         public class ConcreteClassFixture
         {
         }
+
+        public interface IDefaultInterfaceFixture
+        {
+        }
+
+        public class DefaultInterfaceFixture : IDefaultInterfaceFixture
+        {
+        }
+        // ReSharper restore ClassNeverInstantiated.Global
+        // ReSharper restore MemberCanBePrivate.Global
+        // ReSharper restore UnusedType.Global
     }
 }
