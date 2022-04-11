df0b8c62afd5d411013fdd47b55f6b86911ca3ef
Mark Lopez <m@silvenga.com>
2022-04-11 12:32:12 -0500

    Fixed k8s config usage for local development.

--- src/Contrast.K8s.AgentOperator/Startup.cs
+++ src/Contrast.K8s.AgentOperator/Startup.cs
@@ -1,35 +1,54 @@
 ﻿using Autofac;
 using Contrast.K8s.AgentOperator.Autofac;
+using DotnetKubernetesClient;
 using KubeOps.Operator;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Contrast.K8s.AgentOperator
 {
     public class Startup
     {
         private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _environment;
 
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
         {
             _configuration = configuration;
+            _environment = environment;
         }
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddKubernetesOperator();
+            services.AddKubernetesOperator(settings =>
+            {
+                if (_environment.IsDevelopment())
+                {
+                    settings.EnableLeaderElection = false;
+                }
+            });
         }
 
+        // ReSharper disable once UnusedMember.Global
         public void ConfigureContainer(ContainerBuilder builder)
         {
             builder.ApplyContrastConventions(typeof(Startup).Assembly);
+
+            builder.Register(_ => new KubernetesClient()).As<IKubernetesClient>();
         }
 
         public void Configure(IApplicationBuilder app)
         {
             // If needed:
             // var container = app.ApplicationServices.GetAutofacRoot();
+
+            if (_environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
 
             app.UseKubernetesOperator();
         }
