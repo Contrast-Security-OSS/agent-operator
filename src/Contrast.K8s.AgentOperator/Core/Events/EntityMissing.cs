6e360c11de40b30f3e4defd35bca7d460456961d
Mark Lopez <m@silvenga.com>
2022-04-25 10:12:09 -0500

    Dropped superfluous event.

--- src/Contrast.K8s.AgentOperator/Core/Events/EntityMissing.cs
+++
@@ -1,8 +0,0 @@
-﻿using k8s;
-using k8s.Models;
-using MediatR;
-
-namespace Contrast.K8s.AgentOperator.Core.Events
-{
-    public record EntityMissing<T>(string Name, string Namespace) : IRequest where T : IKubernetesObject<V1ObjectMeta>;
-}
