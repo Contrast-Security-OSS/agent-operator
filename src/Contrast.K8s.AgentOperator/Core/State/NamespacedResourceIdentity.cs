0f3f69437312f0b85905152101531a23f7cf8e73
Mark Lopez <m@silvenga.com>
2022-04-25 11:33:28 -0500

    Corrected IResourceWithPodSpec file name.

--- src/Contrast.K8s.AgentOperator/Core/State/NamespacedResourceIdentity.cs
+++ src/Contrast.K8s.AgentOperator/Core/State/NamespacedResourceIdentity.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using Contrast.K8s.AgentOperator.Core.State.Resources.Interfaces;
+using JetBrains.Annotations;
 
 namespace Contrast.K8s.AgentOperator.Core.State
 {
-    public abstract record NamespacedResourceIdentity(string Name, string Namespace, Type Type)
+    public abstract record NamespacedResourceIdentity([UsedImplicitly] string Name, [UsedImplicitly] string Namespace, [UsedImplicitly] Type Type)
     {
         public static NamespacedResourceIdentity<T> Create<T>(string name, string @namespace) where T : INamespacedResource
         {
