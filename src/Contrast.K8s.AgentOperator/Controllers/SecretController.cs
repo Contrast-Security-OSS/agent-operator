b2c79c3cf394b6cf8a0f820eb0699d5eb4e8fa3a
Mark Lopez <m@silvenga.com>
2022-04-27 13:20:55 -0500

    Added missing rbac for MutatingWebhookConfiguration.

--- src/Contrast.K8s.AgentOperator/Controllers/SecretController.cs
+++ src/Contrast.K8s.AgentOperator/Controllers/SecretController.cs
@@ -8,6 +8,7 @@ namespace Contrast.K8s.AgentOperator.Controllers
     [UsedImplicitly]
     // TODO Limit this is only my namespace?
     [EntityRbac(typeof(V1Secret), Verbs = VerbConstants.AllButDelete)]
+    [EntityRbac(typeof(V1MutatingWebhookConfiguration), Verbs = VerbConstants.ReadAndPatch)]
     public class SecretController : GenericController<V1Secret>
     {
         public SecretController(IEventStream eventStream) : base(eventStream)
