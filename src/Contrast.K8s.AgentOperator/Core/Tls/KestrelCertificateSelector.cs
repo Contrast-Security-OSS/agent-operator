a76be87332d90db270d96913a43b8bb1db3dcf67
Mark Lopez <m@silvenga.com>
2022-04-28 15:27:59 -0500

    Added additional logging to sni failures.

--- src/Contrast.K8s.AgentOperator/Core/Tls/KestrelCertificateSelector.cs
+++ src/Contrast.K8s.AgentOperator/Core/Tls/KestrelCertificateSelector.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using NLog;
 
@@ -14,12 +16,21 @@ namespace Contrast.K8s.AgentOperator.Core.Tls
     {
         private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
         private TlsCertificateChain? _chain;
+        private IReadOnlyCollection<string>? _chainSans;
 
         public X509Certificate2? SelectCertificate(string? hostname)
         {
             if (_chain == null)
             {
                 Logger.Warn($"A server certificate was requested for '{hostname}', but none was known at this time.");
+            }
+
+            if (_chain != null
+                && hostname != null
+                && _chainSans != null
+                && !_chainSans.Contains(hostname, StringComparer.OrdinalIgnoreCase))
+            {
+                Logger.Warn($"A server certificate was requested for '{hostname}', but the currently loaded certificate doesn't match.");
             }
 
             return _chain?.ServerCertificate;
@@ -34,9 +45,29 @@ namespace Contrast.K8s.AgentOperator.Core.Tls
             {
                 _chain?.Dispose();
                 _chain = chain;
+                _chainSans = GetSans(_chain.ServerCertificate).ToList();
+
+                Logger.Trace($"Certificate has {_chainSans.Count} SAN's (SAN's: [{string.Join(", ", _chainSans)}]).");
             }
 
             return ownershipTaken;
+        }
+
+        private static IEnumerable<string> GetSans(X509Certificate2 certificate)
+        {
+            var scansLines = certificate.Extensions["2.5.29.17"]?.Format(true);
+            if (scansLines != null)
+            {
+                var sans = scansLines.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (var san in sans)
+                {
+                    var pair = san.Split("=", 2, StringSplitOptions.RemoveEmptyEntries);
+                    if (pair.Length == 2)
+                    {
+                        yield return pair[1].Trim();
+                    }
+                }
+            }
         }
 
         public void Dispose()
