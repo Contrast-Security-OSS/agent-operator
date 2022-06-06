e0dcc4449c9f0a22fdd0e33f4f20b49bb39f4768
Mark Lopez <m@silvenga.com>
2022-06-06 15:05:54 -0500

    Improved the comparer performance.

--- src/Contrast.K8s.AgentOperator/Core/State/ResourceComparer.cs
+++ src/Contrast.K8s.AgentOperator/Core/State/ResourceComparer.cs
@@ -6,16 +6,19 @@ namespace Contrast.K8s.AgentOperator.Core.State
 {
     public interface IResourceComparer
     {
-        bool AreEqual<T>(T left, T right) where T : INamespacedResource;
+        bool AreEqual<T>(T left, T right) where T : INamespacedResource?;
     }
 
     public class ResourceComparer : IResourceComparer
     {
         private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
-        private readonly CompareLogic _compareLogic = new();
+        private readonly CompareLogic _compareLogic = new(new ComparisonConfig
+        {
+            MaxDifferences = 1
+        });
 
-        public bool AreEqual<T>(T left, T right) where T : INamespacedResource
+        public bool AreEqual<T>(T left, T right) where T : INamespacedResource?
         {
             var result = _compareLogic.Compare(left, right);
             if (result.ElapsedMilliseconds > 40)
