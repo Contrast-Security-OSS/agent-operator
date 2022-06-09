0efc97013f063a98b99853f27a525a8279f7abcf
Mark Lopez <m@silvenga.com>
2022-06-09 11:02:15 -0500

    Added tests around configuration optionality.

---
+++ tests/Contrast.K8s.AgentOperator.FunctionalTests/Scenarios/Injection/ConfigOptionalTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Contrast.K8s.AgentOperator.FunctionalTests.Fixtures;
+using FluentAssertions;
+using k8s.Models;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Contrast.K8s.AgentOperator.FunctionalTests.Scenarios.Injection
+{
+    public class ConfigOptionalTests : IClassFixture<TestingContext>
+    {
+        private const string ScenarioName = "config-optional";
+
+        private readonly TestingContext _context;
+
+        public ConfigOptionalTests(TestingContext context, ITestOutputHelper outputHelper)
+        {
+            _context = context;
+            _context.RegisterOutput(outputHelper);
+        }
+
+        [Fact]
+        public async Task When_injected_then_pod_should_have_injection_annotations()
+        {
+            var client = await _context.GetClient();
+
+            // Act
+            var result = await client.GetInjectedPodByPrefix(ScenarioName);
+
+            // Assert
+            result.Annotations().Should().ContainKey("agents.contrastsecurity.com/is-injected").WhoseValue.Should().Be("True");
+        }
+    }
+}
