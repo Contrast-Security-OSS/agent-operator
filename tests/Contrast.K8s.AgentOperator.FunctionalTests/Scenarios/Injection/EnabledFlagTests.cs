ad06fec82fdd49ba29181bbcd7e93a4333087b0d
Mark Lopez <m@silvenga.com>
2022-06-09 11:05:07 -0500

    Added test around enable flag.

---
+++ tests/Contrast.K8s.AgentOperator.FunctionalTests/Scenarios/Injection/EnabledFlagTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Contrast.K8s.AgentOperator.FunctionalTests.Fixtures;
+using FluentAssertions;
+using k8s.Models;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Contrast.K8s.AgentOperator.FunctionalTests.Scenarios.Injection
+{
+    public class EnabledFlagTests : IClassFixture<TestingContext>
+    {
+        private const string ScenarioName = "enabled-flag";
+
+        private readonly TestingContext _context;
+
+        public EnabledFlagTests(TestingContext context, ITestOutputHelper outputHelper)
+        {
+            _context = context;
+            _context.RegisterOutput(outputHelper);
+        }
+
+        [Fact]
+        public async Task When_disabled_then_injector_should_not_inject()
+        {
+            var client = await _context.GetClient();
+
+            // Act
+            var result = await client.GetByPrefix<V1Pod>(ScenarioName);
+
+            // Assert
+            result.Annotations().Should().BeNull();
+        }
+    }
+}
