50f6eeaadb47652e7a39d21a2aec998dfd59df55
Morgan Creekmore <morgan.creekmore@contrastsecurity.com>
2022-08-24 14:36:44 -0500

    Change rbac to use readonly

--- src/Contrast.K8s.AgentOperator/Controllers/DynaKubeController.cs
+++ src/Contrast.K8s.AgentOperator/Controllers/DynaKubeController.cs
@@ -9,7 +9,7 @@ using KubeOps.Operator.Rbac;
 
 namespace Contrast.K8s.AgentOperator.Controllers
 {
-    [EntityRbac(typeof(V1Beta1DynaKube), Verbs = VerbConstants.ReadAndPatch), UsedImplicitly]
+    [EntityRbac(typeof(V1Beta1DynaKube), Verbs = VerbConstants.ReadOnly), UsedImplicitly]
     public class DynaKubeController : GenericController<V1Beta1DynaKube>
     {
         public DynaKubeController(IEventStream eventStream) : base(eventStream)
