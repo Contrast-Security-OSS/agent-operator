c00dcc440388ad6f00cdeabbb9f18cd8aab4cd9e
Morgan Creekmore <morgan.creekmore@contrastsecurity.com>
2022-10-11 16:15:48 -0500

    Add support for patching existing JAVA_TOOL_OPTIONS

---
+++ tests/Contrast.K8s.AgentOperator.FunctionalTests/Scenarios/Injection/Agents/JavaToolOptionsInjectionTests.cs
@@ -0,0 +1,45 @@
+﻿// Contrast Security, Inc licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information.
+
+using System.Threading.Tasks;
+using Contrast.K8s.AgentOperator.FunctionalTests.Fixtures;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Contrast.K8s.AgentOperator.FunctionalTests.Scenarios.Injection.Agents
+{
+    public class JavaToolOptionsInjectionTests : IClassFixture<TestingContext>
+    {
+        private const string ScenarioName = "injection-java-tooloptions";
+
+        private readonly TestingContext _context;
+
+        public JavaToolOptionsInjectionTests(TestingContext context, ITestOutputHelper outputHelper)
+        {
+            _context = context;
+            _context.RegisterOutput(outputHelper);
+        }
+
+        [Fact]
+        public async Task When_injected_then_pod_should_have_patched_java_tool_options_environment_variable()
+        {
+            var client = await _context.GetClient();
+
+            // Act
+            var result = await client.GetInjectedPodByPrefix(ScenarioName);
+
+            // Assert
+            using (new AssertionScope())
+            {
+                var container = result.Spec.Containers.Should().ContainSingle().Subject;
+
+                container.Env.Should().Contain(x => x.Name == "JAVA_TOOL_OPTIONS")
+                    .Which.Value.Should().Be("-javaagent:/opt/contrast/contrast-agent.jar something");
+                container.Env.Should().Contain(x => x.Name == "CONTRAST_EXISTING_JAVA_TOOL_OPTIONS")
+                    .Which.Value.Should().Be("something");
+            }
+        }
+    }
+}
--- tests/Contrast.K8s.AgentOperator.Tests/Core/Reactions/Injecting/Patching/Agents/JavaAgentPatcherTests.cs
+++ tests/Contrast.K8s.AgentOperator.Tests/Core/Reactions/Injecting/Patching/Agents/JavaAgentPatcherTests.cs
@@ -7,6 +7,7 @@ using Contrast.K8s.AgentOperator.Core.Reactions.Injecting.Patching;
 using Contrast.K8s.AgentOperator.Core.Reactions.Injecting.Patching.Agents;
 using Contrast.K8s.AgentOperator.Core.State.Resources.Primitives;
 using FluentAssertions;
+using FluentAssertions.Execution;
 using k8s.Models;
 using Xunit;
 
@@ -55,5 +56,117 @@ namespace Contrast.K8s.AgentOperator.Tests.Core.Reactions.Injecting.Patching.Age
                 new("JAVA_TOOL_OPTIONS", $"-javaagent:{contextFake.ContrastMountPath}/contrast-agent.jar")
             });
         }
+
+        [Fact]
+        public void PatchContainer_should_not_do_anything_if_we_already_injected()
+        {
+            var patcher = new JavaAgentPatcher();
+            var context = AutoFixture.Create<PatchingContext>();
+            var existingToolOptions = "-javaagent:/somepath/contrast-agent.jar";
+            var container = AutoFixture.Build<V1Container>()
+                .With(x => x.Env, new List<V1EnvVar> { new("JAVA_TOOL_OPTIONS", existingToolOptions) }).Create();
+
+            // Act
+            patcher.PatchContainer(container, context);
+
+            // Assert
+            using (new AssertionScope())
+            {
+                container.Env.Should().NotContain(x => x.Name == "CONTRAST_EXISTING_JAVA_TOOL_OPTIONS");
+                container.Env.Should().Contain(x => x.Name == "JAVA_TOOL_OPTIONS" && x.Value == existingToolOptions);
+            }
+        }
+
+        [Fact]
+        public void PatchContainer_should_handle_existing_javaagent()
+        {
+            var patcher = new JavaAgentPatcher();
+            var context = AutoFixture.Create<PatchingContext>();
+            var existingToolOptions = "-javaagent:/somepath/some-tool.jar -Dcontrast.dir=/tmp";
+            var container = AutoFixture.Build<V1Container>()
+                .With(x => x.Env, new List<V1EnvVar> { new("JAVA_TOOL_OPTIONS", existingToolOptions) }).Create();
+
+            // Act
+            patcher.PatchContainer(container, context);
+
+            // Assert
+            var expectedToolOptions = $"-javaagent:{context.ContrastMountPath}/contrast-agent.jar -javaagent:/somepath/some-tool.jar -Dcontrast.dir=/tmp";
+            using (new AssertionScope())
+            {
+                container.Env.Should()
+                    .Contain(x => x.Name == "CONTRAST_EXISTING_JAVA_TOOL_OPTIONS" && x.Value == existingToolOptions);
+                container.Env.Should()
+                    .Contain(x => x.Name == "JAVA_TOOL_OPTIONS" && x.Value == expectedToolOptions);
+            }
+        }
+
+        [Fact]
+        public void PatchContainer_should_handle_existing_correct_contrast_javaagent()
+        {
+            var patcher = new JavaAgentPatcher();
+            var context = AutoFixture.Create<PatchingContext>();
+            var existingToolOptions = "-javaagent:/somepath/contrast-agent.jar -Dcontrast.dir=/tmp";
+            var container = AutoFixture.Build<V1Container>()
+                .With(x => x.Env, new List<V1EnvVar> { new("JAVA_TOOL_OPTIONS", existingToolOptions) }).Create();
+
+            // Act
+            patcher.PatchContainer(container, context);
+
+            // Assert
+            var expectedToolOptions = $"-javaagent:{context.ContrastMountPath}/contrast-agent.jar -Dcontrast.dir=/tmp";
+            using (new AssertionScope())
+            {
+                container.Env.Should()
+                    .Contain(x => x.Name == "CONTRAST_EXISTING_JAVA_TOOL_OPTIONS" && x.Value == existingToolOptions);
+                container.Env.Should()
+                    .Contain(x => x.Name == "JAVA_TOOL_OPTIONS" && x.Value == expectedToolOptions);
+            }
+        }
+
+        [Fact]
+        public void PatchContainer_should_handle_no_javaagent()
+        {
+            var patcher = new JavaAgentPatcher();
+            var context = AutoFixture.Create<PatchingContext>();
+            var existingToolOptions = "-Dcontrast.dir=/tmp";
+            var container = AutoFixture.Build<V1Container>()
+                .With(x => x.Env, new List<V1EnvVar> { new("JAVA_TOOL_OPTIONS", existingToolOptions) }).Create();
+
+            // Act
+            patcher.PatchContainer(container, context);
+
+            // Assert
+            var expectedToolOptions = $"-javaagent:{context.ContrastMountPath}/contrast-agent.jar -Dcontrast.dir=/tmp";
+            using (new AssertionScope())
+            {
+                container.Env.Should()
+                    .Contain(x => x.Name == "CONTRAST_EXISTING_JAVA_TOOL_OPTIONS" && x.Value == existingToolOptions);
+                container.Env.Should()
+                    .Contain(x => x.Name == "JAVA_TOOL_OPTIONS" && x.Value == expectedToolOptions);
+            }
+        }
+
+        [Fact]
+        public void PatchContainer_should_not_patch_on_malformed_options()
+        {
+            var patcher = new JavaAgentPatcher();
+            var context = AutoFixture.Create<PatchingContext>();
+            var existingToolOptions = "-Dcontrast.dir='/tmp";
+            var container = AutoFixture.Build<V1Container>()
+                .With(x => x.Env, new List<V1EnvVar> { new("JAVA_TOOL_OPTIONS", existingToolOptions) }).Create();
+
+            // Act
+            patcher.PatchContainer(container, context);
+
+            // Assert
+            using (new AssertionScope())
+            {
+                container.Env.Should()
+                    .Contain(x => x.Name == "CONTRAST_EXISTING_JAVA_TOOL_OPTIONS" && x.Value == existingToolOptions);
+                container.Env.Should()
+                    .Contain(x => x.Name == "JAVA_TOOL_OPTIONS" && x.Value == existingToolOptions);
+            }
+        }
+
     }
 }
