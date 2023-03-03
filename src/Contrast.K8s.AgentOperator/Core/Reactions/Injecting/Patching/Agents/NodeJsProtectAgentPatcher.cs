7350100138ddebfc2946cec544dd9983478a1a51
Mark Lopez <m@silvenga.com>
2023-03-03 11:17:37 -0600

    Added redirect for node security logs to the writable mount.

--- src/Contrast.K8s.AgentOperator/Core/Reactions/Injecting/Patching/Agents/NodeJsProtectAgentPatcher.cs
+++ src/Contrast.K8s.AgentOperator/Core/Reactions/Injecting/Patching/Agents/NodeJsProtectAgentPatcher.cs
@@ -16,6 +16,7 @@ namespace Contrast.K8s.AgentOperator.Core.Reactions.Injecting.Patching.Agents
             // https://nodejs.org/api/cli.html#node_optionsoptions
             yield return new V1EnvVar("NODE_OPTIONS", $"--require {context.AgentMountPath}/node_modules/@contrast/protect-agent");
             yield return new V1EnvVar("CONTRAST__AGENT__LOGGER__PATH", $"{context.WritableMountPath}/logs/contrast_agent.log");
+            yield return new V1EnvVar("CONTRAST__AGENT__SECURITY_LOGGER__PATH", $"{context.WritableMountPath}/logs/contrast_agent_cef.log");
         }
     }
 }
