04596380ff775e922bc7e94dc0c2996f12008102
ContrastSecu <102801758+ContrastSecu@users.noreply.github.com>
2023-09-19 22:31:16 +0200

    Update RolloutController.cs

--- src/Contrast.K8s.AgentOperator/Controllers/RolloutController.cs
+++ src/Contrast.K8s.AgentOperator/Controllers/RolloutController.cs
@@ -8,19 +8,14 @@ using JetBrains.Annotations;
 using k8s.Models;
 using KubeOps.Operator.Controller;
 using KubeOps.Operator.Rbac;
-using NLog;
 
 namespace Contrast.K8s.AgentOperator.Controllers
 {
     //[EntityRbac(typeof(V1alpha1Rollout), Verbs = VerbConstants.ReadAndPatch), UsedImplicitly]
     public class RolloutController : GenericController<V1alpha1Rollout> //IResourceController<V1alpha1Rollout>
     {
-        //private readonly IEventStream _eventStream;
-        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
         public RolloutController(IEventStream eventStream) : base(eventStream)
         {
-            //_eventStream = eventStream;
-            Logger.Info($"RolloutController {eventStream.ToString()}");
         }
     }
 }
