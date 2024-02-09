88b3a081f9cb004158e3750ee7b46dd9ec092205
Morgan Creekmore <morgan.creekmore@contrastsecurity.com>
2024-02-08 18:34:40 -0600

    Fix nodejs-esm test

--- tests/Contrast.K8s.AgentOperator.FunctionalTests/Scenarios/Injection/Agents/NodeJsEsmInjectionTests.cs
+++ tests/Contrast.K8s.AgentOperator.FunctionalTests/Scenarios/Injection/Agents/NodeJsEsmInjectionTests.cs
@@ -35,7 +35,7 @@ namespace Contrast.K8s.AgentOperator.FunctionalTests.Scenarios.Injection.Agents
             {
                 var container = result.Spec.Containers.Should().ContainSingle().Subject;
                 container.Env.Should().Contain(x => x.Name == "NODE_OPTIONS")
-                         .Which.Value.Should().Be("--import /contrast/agent/node_modules/@contrast/agent");
+                         .Which.Value.Should().Be("--import /contrast/agent/node_modules/@contrast/agent/lib/esm-loader.mjs");
                 container.Env.Should().Contain(x => x.Name == "CONTRAST__AGENT__LOGGER__PATH")
                          .Which.Value.Should().Be("/contrast/data/logs/contrast_agent.log");
             }
