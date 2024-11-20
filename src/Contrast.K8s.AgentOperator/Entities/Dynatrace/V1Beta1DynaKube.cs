4b82c44d3ceca6de6cbf65202a19aba5722084e7
Morgan Creekmore <morgan.creekmore@contrastsecurity.com>
2024-11-20 15:54:04 -0600

    Add ignore attributes for crd we dont own, change to description attribute to support new crd generator

--- src/Contrast.K8s.AgentOperator/Entities/Dynatrace/V1Beta1DynaKube.cs
+++ src/Contrast.K8s.AgentOperator/Entities/Dynatrace/V1Beta1DynaKube.cs
@@ -5,22 +5,18 @@ using System.Text.Json.Serialization;
 using Contrast.K8s.AgentOperator.Core.Kube;
 using k8s.Models;
 using KubeOps.Abstractions.Entities;
+using KubeOps.Abstractions.Entities.Attributes;
 using KubeOps.Abstractions.Rbac;
 
 namespace Contrast.K8s.AgentOperator.Entities.Dynatrace;
 
+[Ignore] //Don't generate a CRD for this
 [KubernetesEntity(Group = "dynatrace.com", ApiVersion = "v1beta1", Kind = "DynaKube", PluralName = "dynakubes")]
 [EntityRbac(typeof(V1Beta1DynaKube), Verbs = VerbConstants.ReadOnly)]
 public partial class V1Beta1DynaKube : CustomKubernetesEntity<V1Beta1DynaKube.DynaKubeSpec>
 {
-    /// <summary>
-    /// DynaKubeSpec represents the dynatrace operator config
-    /// </summary>
     public class DynaKubeSpec
     {
-        /// <summary>
-        /// OneAgent Config
-        /// </summary>
         [JsonPropertyName("oneAgent")]
         public OneAgentSpec? OneAgent { get; set; }
     }
--- src/Contrast.K8s.AgentOperator/Entities/OpenShift/V1DeploymentConfig.cs
+++ src/Contrast.K8s.AgentOperator/Entities/OpenShift/V1DeploymentConfig.cs
@@ -6,28 +6,21 @@ using System.Text.Json.Serialization;
 using Contrast.K8s.AgentOperator.Core.Kube;
 using k8s.Models;
 using KubeOps.Abstractions.Entities;
+using KubeOps.Abstractions.Entities.Attributes;
 using KubeOps.Abstractions.Rbac;
 
 namespace Contrast.K8s.AgentOperator.Entities.OpenShift;
 
+[Ignore] //Don't generate a CRD for this
 [KubernetesEntity(Group = "apps.openshift.io", ApiVersion = "v1", Kind = "DeploymentConfig", PluralName = "deploymentconfigs")]
 [EntityRbac(typeof(V1DeploymentConfig), Verbs = VerbConstants.ReadAndPatch)]
 public partial class V1DeploymentConfig : CustomKubernetesEntity<V1DeploymentConfig.DeploymentConfigSpec>
 {
-    /// <summary>
-    /// DeploymentConfigSpec represents the desired state of the deployment.
-    /// </summary>
     public class DeploymentConfigSpec
     {
-        /// <summary>
-        /// Selector is a label query over pods that should match the Replicas count.
-        /// </summary>
         [JsonPropertyName("selector")]
         public IDictionary<string, string> Selector { get; set; } = new Dictionary<string, string>();
 
-        /// <summary>
-        /// Template is the object that describes the pod that will be created if insufficient replicas are detected.
-        /// </summary>
         [JsonPropertyName("template")]
         public V1PodTemplateSpec? Template { get; set; }
     }
