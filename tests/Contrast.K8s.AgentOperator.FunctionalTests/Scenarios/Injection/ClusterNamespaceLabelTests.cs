ccc8933b1e56a0267f6494e45b70ea67bdd558de
Morgan Creekmore <morgan.creekmore@contrastsecurity.com>
2025-08-15 15:10:36 -0500

    Update docs, add functional tests

---
+++ tests/Contrast.K8s.AgentOperator.FunctionalTests/Scenarios/Injection/ClusterNamespaceLabelTests.cs
@@ -0,0 +1,36 @@
+﻿// Contrast Security, Inc licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information.
+
+using System.Threading.Tasks;
+using Contrast.K8s.AgentOperator.FunctionalTests.Fixtures;
+using FluentAssertions;
+using k8s.Models;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Contrast.K8s.AgentOperator.FunctionalTests.Scenarios.Injection;
+
+public class ClusterNamespaceLabelTests : IClassFixture<TestingContext>
+{
+    private const string ScenarioName = "injection-namespacelabel";
+
+    private readonly TestingContext _context;
+
+    public ClusterNamespaceLabelTests(TestingContext context, ITestOutputHelper outputHelper)
+    {
+        _context = context;
+        _context.RegisterOutput(outputHelper);
+    }
+
+    [Fact]
+    public async Task When_injected_then_pod_should_have_injection_annotations()
+    {
+        var client = await _context.GetClient(defaultNamespace: "testing-namespacelabel");
+
+        // Act
+        var result = await client.GetInjectedPodByPrefix(ScenarioName);
+
+        // Assert
+        result.Annotations().Should().ContainKey("agents.contrastsecurity.com/is-injected").WhoseValue.Should().Be("True");
+    }
+}
