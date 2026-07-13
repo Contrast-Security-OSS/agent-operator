8e0a593a907de2d86987dc851fbecbf586c464d6
Brian Hawley <2397784+nairb@users.noreply.github.com>
2026-07-13 14:08:50 -0500

    feat: carry ReconcilePolicy on AgentInjectorResource, excluded from hash

--- tests/Contrast.K8s.AgentOperator.Tests/Core/State/ResourceHasherTests.cs
+++ tests/Contrast.K8s.AgentOperator.Tests/Core/State/ResourceHasherTests.cs
@@ -1,10 +1,12 @@
 ﻿// Contrast Security, Inc licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information.
 
+using System.Linq;
 using AutoFixture;
 using Contrast.K8s.AgentOperator.Core.Kube;
 using Contrast.K8s.AgentOperator.Core.State;
 using Contrast.K8s.AgentOperator.Core.State.Resources;
+using Contrast.K8s.AgentOperator.Core.State.Resources.Primitives;
 using FluentAssertions;
 using Xunit;
 
@@ -29,6 +31,38 @@ namespace Contrast.K8s.AgentOperator.Tests.Core.State
 
             // Assert
             result.Should().NotBeNullOrWhiteSpace();
+        }
+
+        [Fact]
+        public void GetHash_is_identical_regardless_of_reconcile_policy()
+        {
+            var always = AutoFixture.Create<AgentInjectorResource>() with { ReconcilePolicy = ReconcilePolicy.Always };
+            var onCreate = always with { ReconcilePolicy = ReconcilePolicy.OnCreate };
+            var connection = AutoFixture.Create<AgentConnectionResource>();
+            var configuration = AutoFixture.Create<AgentConfigurationResource>();
+            var secrets = AutoFixture.CreateMany<SecretResource>().ToList();
+            var hasher = CreateGraph();
+
+            var alwaysHash = hasher.GetHash(always, connection, configuration, secrets);
+            var onCreateHash = hasher.GetHash(onCreate, connection, configuration, secrets);
+
+            onCreateHash.Should().Be(alwaysHash);
+        }
+
+        [Fact]
+        public void GetHash_still_changes_when_a_hashed_field_changes()
+        {
+            var injector = AutoFixture.Create<AgentInjectorResource>() with { Enabled = true };
+            var flipped = injector with { Enabled = false };
+            var connection = AutoFixture.Create<AgentConnectionResource>();
+            var configuration = AutoFixture.Create<AgentConfigurationResource>();
+            var secrets = AutoFixture.CreateMany<SecretResource>().ToList();
+            var hasher = CreateGraph();
+
+            var original = hasher.GetHash(injector, connection, configuration, secrets);
+            var changed = hasher.GetHash(flipped, connection, configuration, secrets);
+
+            changed.Should().NotBe(original);
         }
 
         private static IResourceHasher CreateGraph(KubernetesJsonSerializer? jsonSerializer = null)
