8d4bcd4915b9ce7dc2f50b16f10487383848e5bf
Morgan Creekmore <morgan.creekmore@contrastsecurity.com>
2026-07-17 15:36:46 -0500

    Add functional tests for reconcilePolicy: OnCreate

---
+++ tests/Contrast.K8s.AgentOperator.FunctionalTests/Scenarios/Injection/ReconcileOnCreateTests.cs
@@ -0,0 +1,36 @@
+﻿// Contrast Security, Inc licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information.
+
+using System.Threading.Tasks;
+using Contrast.K8s.AgentOperator.FunctionalTests.Fixtures;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Contrast.K8s.AgentOperator.FunctionalTests.Scenarios.Injection;
+
+public class ReconcileOnCreateTests : IClassFixture<TestingContext>
+{
+    private const string ScenarioName = "reconcile-oncreate";
+
+    private readonly TestingContext _context;
+
+    public ReconcileOnCreateTests(TestingContext context, ITestOutputHelper outputHelper)
+    {
+        _context = context;
+        _context.RegisterOutput(outputHelper);
+    }
+
+    [Fact]
+    public async Task Pod_has_volume_and_config_env_var()
+    {
+        var client = await _context.GetClient();
+
+        // Act
+        var result = await client.GetInjectedPodByPrefix(ScenarioName);
+
+        // Assert
+        result.Spec.InitContainers.Should().ContainSingle(x => x.Name == "contrast-init")
+              .Which.Image.Should().Be("contrast/agent-dummy:latest");
+    }
+}
